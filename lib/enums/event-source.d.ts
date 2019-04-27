ac92036f6f89cefdec337f1ed174c5a57f28227a
Michael Schilling <mschilling@move4mobile.com>
2019-04-27 15:49:27 +0200

    Release 0.0.3

---
+++ lib/enums/event-source.d.ts
@@ -0,0 +1,6 @@
+export declare enum EventSource {
+    GITHUB = "GITHUB",
+    TRAVIS = "TRAVIS",
+    FRESHDESK = "FRESHDESK",
+    JIRA = "JIRA"
+}
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,2 @@
-export * from './interfaces/user';
+export * from './enums';
+export * from './interfaces';
