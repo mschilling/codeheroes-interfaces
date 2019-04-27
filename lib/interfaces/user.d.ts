2773989e720f22be193083d773f090357e6b058a
Michael Schilling <mschilling@move4mobile.com>
2019-04-27 15:42:21 +0200

    Release 0.0.2

---
+++ lib/interfaces/user.d.ts
@@ -0,0 +1,10 @@
+export interface IUser {
+    id: string;
+    name: string;
+    email: string;
+    avatar: string;
+    xp: number;
+    totalXp: number;
+    rank: number;
+    active: boolean;
+}
